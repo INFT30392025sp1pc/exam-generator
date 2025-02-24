07552df3e0c74da4cbaf7002e71e83ebf7e3a425
waltg003 <waltg003@mymail.unisa.edu.au>
2025-02-24 21:08:56 +1030

    Tidied up db config master

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "ls-d569aff0f012a672548dfa141c2ff6da29f4a426.cx4g6oyqyv3o.ap-southeast-2.rds.amazonaws.com"; // Change if necessary
-$username = "dbmasteruser"; // Change if necessary
-$password = ",urI{;P+E-97C<Is>,AZkgnDZ5V313mw"; // Change if necessary
-$dbname = "dbmaster"; // Change to your actual database name
+$servername = "ls-d569aff0f012a672548dfa141c2ff6da29f4a426.cx4g6oyqyv3o.ap-southeast-2.rds.amazonaws.com";
+$username = "dbmasteruser"; 
+$password = ",urI{;P+E-97C<Is>,AZkgnDZ5V313mw"; 
+$dbname = "dbmaster"; 
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
