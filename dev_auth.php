0c1aedc3add7fd99e97d22fbe1a607a753f4222c
Wei <linwy029@mymail.unisa.edu.au>
2025-04-24 12:00:28 +0800

    Add dev_auth
    this file allow bypass of login requirement

---
+++ dev_auth.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * This file is used to bypass login requirements
+ * in local testing environment
+ * when tester is trying to access a local app file
+ * that requires authentication
+ * it can done by entering
+ * "http://localhost/exam-generator/dev_auth.php?return=/path/to/protected.php"
+ * in browser and replace the end after "return" to the path to
+ * the file being tested
+ */
+
+session_start();
+$_SESSION['username'] = 'devuser';
+$_SESSION['authenticated'] = true;
+header("Location: ".$_GET['return'] ?? '/');
