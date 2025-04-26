7b0debca4377c6cd304649422d79629b9e512ad8
Steven Davidson <davsy036@mymail.unisa.edu.au>
2025-04-26 15:21:42 +1000

    KAN-319 #time 0d 3h 15m #comment Create and push db_localhost.php to prod

--- db_localhost.php
+++ db_localhost.php
@@ -1,18 +1,23 @@
 <?php
-$host="ls-d569aff0f012a672548dfa141c2ff6da29f4a426.cx4g6oyqyv3o.ap-southeast-2.rds.amazonaws.com";
-$port=3306;
-$socket="";
-$user="dbmasteruser";
-$password="";
-$dbname="dbmaster";
+// Enable MySQLi error reporting for development
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-$con = new mysqli($host, $user, $password, $dbname, $port, $socket)
-	or die ('Could not connect to the database server' . mysqli_connect_error());
+// Database connection parameters
+$host = "ls-d569aff0f012a672548dfa141c2ff6da29f4a426.cx4g6oyqyv3o.ap-southeast-2.rds.amazonaws.com";
+$port = 3306;
+$user = "dbmasteruser";
+$password = ",urI{;P+E-97C<Is>,AZkgnDZ5V313mw"; 
+$dbname = "Database-1";
 
-//$con->close();
+// Create a new MySQLi connection
+$con = new mysqli($host, $user, $password, $dbname, $port);
+
+// Check connection
 if ($con->connect_error) {
     die("Connection failed: " . $con->connect_error);
-} else {
-    //echo "Connected successfully";
 }
+
+// Connection successful
+echo "Connected successfully";
 ?>
+
