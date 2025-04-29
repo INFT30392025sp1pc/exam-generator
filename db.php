04b0362335f1a8d167e23991ed2edb8cb4ae3e1d
jmsgrdnr1 <jmsgrdnr1@gmail.com>
2025-04-29 12:46:29 +1000

    Update db.php
    Just a roll back to the previous working code for a stakeholder demonstration prior to an update.

--- db.php
+++ db.php
@@ -1,1 +1,14 @@
-<?php $conn = new mysqli("localhost", "root", "", "exam-generator-testdb");
+<?php
+$servername = "ls-d569aff0f012a672548dfa141c2ff6da29f4a426.cx4g6oyqyv3o.ap-southeast-2.rds.amazonaws.com";
+$username = "dbmasteruser"; 
+$password = ",urI{;P+E-97C<Is>,AZkgnDZ5V313mw"; 
+$dbname = "dbmaster"; 
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
