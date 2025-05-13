5c32532becb783dc54539d629655e0788a67bb31
Wei <linwy029@mymail.unisa.edu.au>
2025-05-13 21:44:02 +0800

    Add failed login and SQL injection
    - attempt to enter invalid credentials
    - attempt using SQL injection in credential fields

--- tests/Acceptance/loginCest.php
+++ tests/Acceptance/loginCest.php
@@ -34,4 +34,53 @@ final class loginCest
         $I->amOnPage('/dashboard.php');
     }
 
+    public function testUnSuccessfulLogin2(AcceptanceTester $I): void
+    {
+        $I->amOnPage('/login.php');
+        $I->see('Login');
+        $I->fillField('username', 'test');
+        $I->fillField('password', '123');
+        $I->click('Login');
+        $I->see('Invalid username or password!');
+        $I->amOnPage('/login.php');
+    }
+
+    public function testSQLInjection1(AcceptanceTester $I): void
+    {
+        $I->amOnPage('/login.php');
+        $I->see('Login');
+        $I->fillField('username', 'INSERT INTO table_name (column1, column2, column3)
+        VALUES (value1, value2, value3);');
+        $I->fillField('password', '');
+        $I->click('Login');
+        $I->see('Invalid username or password!');
+        $I->amOnPage('/login.php');
+    }
+
+    public function testSQLInjection2(AcceptanceTester $I): void
+    {
+        $I->amOnPage('/login.php');
+        $I->see('Login');
+
+        //Try comment out the rest of the query nullifying password checks
+        $I->fillField('username', "' --admin' /*");
+        $I->fillField('password', '');
+        $I->click('Login');
+        $I->see('Invalid username or password!');
+        $I->amOnPage('/login.php');
+    }
+
+    public function testSQLInjection3(AcceptanceTester $I): void
+    {
+        $I->amOnPage('/login.php');
+        $I->see('Login');
+
+        //Try use or keyword to make the where clause always true
+        $I->fillField('username', "' OR '1'='1");
+        $I->fillField('password', '');
+        $I->click('Login');
+        $I->see('Invalid username or password!');
+        $I->amOnPage('/login.php');
+    }
+
 }
